5204b199353f2f6bf04fc11071c378c59e59a763
jdevaiya <jayendradevaiya@gmail.com>
2025-09-23 22:10:57 +0530

    blog Project

---
+++ Megaproject/src/pages/Login.jsx
@@ -0,0 +1,11 @@
+import { Login as loginComponent } from "../components";
+import React from "react";
+function Login(){
+    return(
+        <div className=" py-8">
+            <loginComponent />
+        </div>
+    )
+}
+
+export default Login
